1e64cae313728f3d6d910c82c11466e9892417a1
i.chebotarev <ilya.chebotarev@my.games>
2025-10-08 13:07:42 +0300

    remove unused assets, configuration files, and add initial project setup

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 8080,
+    open: true,
+  },
+  plugins: [viteSingleFile()],
+  build: {
+    outDir: "dist",
+    assetsInlineLimit: 100000000, // inline all assets (100MB limit)
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
+    },
+  },
+});
